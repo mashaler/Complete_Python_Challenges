29c6b389f778006acda78147a8cdc792ae9b2859
mashaler <mashalerolva@gmail.com>
2023-01-22 13:22:53 +0200

    third challenge

---
+++ challenge_3.py
@@ -0,0 +1,8 @@
+#write a program that, asks the user to input the width and height,calculates the area, outputs the result.
+
+width = int(input("Enter the width:"))
+height = int(input("Enter the height:"))
+
+area = width * height
+
+print("Your area is:",area)
