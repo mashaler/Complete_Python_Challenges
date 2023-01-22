7bca48108875377c2e0ed0a630d5048082a912de
mashaler <mashalerolva@gmail.com>
2023-01-22 13:07:29 +0200

    second challenge

---
+++ challenge_2.py
@@ -0,0 +1,8 @@
+#write a program that, asks the user to input two numbers,calculates the average,outputs the result.
+
+number1 = int(input("Enter first number:"))
+number2 = int(input("Enter second number:"))
+
+average = (number1 + number2) / 2
+
+print("Your average is: ",str(average))
