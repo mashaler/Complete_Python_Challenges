b8c1cd31e33a9838753f59721b6de3b625e2f82e
mashaler <mashalerolva@gmail.com>
2023-01-22 13:50:38 +0200

    6th commit

---
+++ challenge_6.py
@@ -0,0 +1,12 @@
+#write a program that: asks the user their name, 
+# if it is the same as your name, outputs 'you're cool',
+#  otherwise outputs 'Nice to meet you'.
+
+name = "Rolva"
+
+user = input("Enter your name: ")
+
+if user == name:
+    print("You're cool")
+else:
+    print("Nice to meet you")
