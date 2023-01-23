69c7cb04eb3712dddbbfd0f154f2c1efb684ec22
mashaler <mashalerolva@gmail.com>
2023-01-23 21:40:52 +0200

    Challenge 11

---
+++ challenge_11.py
@@ -0,0 +1,9 @@
+#write a program that: asks the user to input a sentence
+#calculates and outputs how many characters there are in the sentence
+
+#!/usr/bin/python3
+
+#Asks the user to input a sentence
+sentence = input("Enter a sentence:")
+
+print("There are ",len(sentence),"characters")
